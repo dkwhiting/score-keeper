387e91bd36b1124bd4c4afbb7c8c3972136bde58
dkwhiting <dallinkwhiting@gmail.com>
2023-01-16 16:51:29 -0700

    Added 404 not found route

--- src/components/View.js
+++ src/components/View.js
@@ -5,6 +5,7 @@ import {
 } from 'react-router-dom'
 import Scoreboard from './Scoreboard';
 import Setup from './Setup';
+import NotFound from './NotFound';
 
 const View = () => {
   const [players, setPlayers] = useState(JSON.parse(localStorage.getItem('players')))
@@ -12,12 +13,16 @@ const View = () => {
   return (
     <Routes>
       <Route
-        exact path="score-keeper/"
+        exact path="/"
         element={<Setup players={players} setPlayers={setPlayers} />}
       />
       <Route
         exact path="score-keeper/scoreboard"
         element={<Scoreboard players={players} setPlayers={setPlayers} />}
+      />
+      <Route
+        exact path="/*"
+        element={<NotFound />}
       />
     </Routes>
   );
